f811d5bcdc0f6f4c25e36255ea025e65686d18f3
myrzabek2022 <myrzabek2022@gmail.com>
2022-01-26 20:53:58 +0600

    foreach

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        tapshyrma1
+//        System.out.println(summa(3,5,7,7,4));
+//    }
+//    static int summa(int ...kopsan){
+//        int counter=0;
+//        for (int i = 0; i < kopsan.length; i++) {
+//            counter=counter+kopsan[i];
+//        }
+//        return counter;
+//        tapshyrma 2
+//        String[]attar={"rozaana","alina","apake"};
+//        for(String a:attar)
+//            System.out.println("men seni jakshy korom"+" "+a);
+//        Scanner soz=new Scanner(System.in);
+//        String s=soz.nextLine();
+//        System.out.println(s);
+//        System.out.println(s.toUpperCase());
+//        System.out.println(s.trim());
+//        if ()
+//        System.out.println();
+
+    }
+}
